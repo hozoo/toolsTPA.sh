30c174d8032c36a41ad9c5f467b298ad7b4862f9
hozoo <143112689+hozoo@users.noreply.github.com>
2023-10-20 00:18:23 +0700

    SC hozoo saling follow gw 😱

---
+++ toolsTPA.sh
@@ -0,0 +1,101 @@
+#batas gan
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+figlet "hozo "
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Hozoo👑"
+echo $cy" Tiktok:hackershozoo"$pur"✓"
+echo $cy" Whatsapp :08999859595"$pur"✓"
+echo $cy"-----------------"$pur"-------------------" 
+sleep 2
+echo $bi "Pilih" $i "Yang" $cy "Mana" $pu "[?]"
+sleep 1
+echo [1]Clear
+echo [2]login termux
+echo [3]buka aplikasi Tiktok
+echo [4]join grup hackershozoo
+echo [5]membuat text ascii
+echo [6]membuka akun github anda
+echo [7]akun github admin
+echo [8] membuka vidio jj hackershozoo
+echo [9] subcribe hackershozoo
+read -p "[?] Choice : " pil :
+#Batas Gan
+if [ $pil = 1 ]
+then
+clear
+sleep 1
+figlet "cleared"
+sleep 1
+echo "Terimakasih Telah Mencoba Script Kami"
+echo "Jangan Lupa follow Channel hackershozoo"
+sleep 1
+echo $ku "Byee"
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+clear
+login
+sleep 1
+figlet "logged"
+sleep 2
+echo "terimakasih telah mencoba script kami"
+sleep 1
+echo "jangan lupa follow channel hackershozoo"
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+xdg-open https://www.tiktok.com/@hackershozoo?_t=8fsVbQZgx2w&_r=1
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 4 ]
+then
+xdg-open https://chat.whatsapp.com/KHj7b2WDMfn03aaY90XYqn
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 5 ]
+then
+xdg-open http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 6 ]
+then
+xdg-open https://github.com
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 7 ]
+then
+xdg-open https://github.com/hozo
+sleep 2
+sh toolsTPA.sh
+fi
+#batas Gan
+if  [ $pil = 8 ]
+then
+xdg-open https://youtube.com/shorts/9k9szEtUPZw?si=yzzvRKtryzxJRhYq
+sleep2
+sh toolsTPA
+fi
+#batas Gan
+if  [ $pil  = 9 ]
+then
+xdg-open https://youtube.com/@hackerHozoo-vx2uo?si=wOVnRsMrIhaRrmFX
+clear
+fi
+       
